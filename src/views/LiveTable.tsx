56cdc573cbc5afc90063e28b09aa1c26f3488097
Steven Hankin <steven.hankin@gmail.com>
2025-10-15 16:18:40 +0100

    Aggrid table

--- src/views/LiveTable.tsx
+++ src/views/LiveTable.tsx
@@ -1,24 +1,33 @@
 import type { SymbolRow } from '../api/types';
+import {
+  AllCommunityModule,
+  ModuleRegistry,
+  type ColDef
+} from 'ag-grid-community';
+import { AgGridReact } from 'ag-grid-react';
+
+// Register all Community features
+ModuleRegistry.registerModules([AllCommunityModule]);
 
 export const LiveTable = ({ rows }: { rows: SymbolRow[] }) => {
+  // Column Definitions: Defines the columns to be displayed.
+  const colDefs: ColDef[] = [
+    { field: 'symbol', headerName: 'Symbol' },
+    {
+      field: 'price',
+      headerName: 'Price',
+      valueFormatter: (p) => p.value.toFixed(2)
+    },
+    {
+      field: 'ts',
+      headerName: 'Time',
+      valueFormatter: (p) => new Date(p.value).toLocaleTimeString()
+    }
+  ];
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Symbol</th>
-          <th>Price</th>
-          <th>Time</th>
-        </tr>
-      </thead>
-      <tbody>
-        {rows.map((r) => (
-          <tr key={r.symbol}>
-            <td>{r.symbol}</td>
-            <td>{r.price.toFixed(2)}</td>
-            <td>{new Date(r.ts).toLocaleTimeString()}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div style={{ height: '80vh' }}>
+      <AgGridReact rowData={rows} columnDefs={colDefs} domLayout='autoHeight' />
+    </div>
   );
 };
