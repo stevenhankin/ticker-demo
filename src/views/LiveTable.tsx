2d97c5cee0f0ce5055742289583afbae9947b804
Steven Hankin <steven.hankin@gmail.com>
2025-10-15 18:18:12 +0100

    AgGrid Quartz theme

--- src/views/LiveTable.tsx
+++ src/views/LiveTable.tsx
@@ -1,14 +1,20 @@
-import type { SymbolRow } from '../api/types';
 import {
   AllCommunityModule,
   ModuleRegistry,
+  themeQuartz,
   type ColDef
 } from 'ag-grid-community';
 import { AgGridReact } from 'ag-grid-react';
+import type { SymbolRow } from '../api/types';
 
 // Register all Community features
 ModuleRegistry.registerModules([AllCommunityModule]);
 
+/**
+ * AGGrid table for live symbol data
+ *
+ * See [AgGrid React](https://www.ag-grid.com/react-data-grid/getting-started/)
+ */
 export const LiveTable = ({ rows }: { rows: SymbolRow[] }) => {
   // Column Definitions: Defines the columns to be displayed.
   const colDefs: ColDef[] = [
@@ -26,8 +32,13 @@ export const LiveTable = ({ rows }: { rows: SymbolRow[] }) => {
   ];
 
   return (
-    <div style={{ height: '80vh' }}>
-      <AgGridReact rowData={rows} columnDefs={colDefs} domLayout='autoHeight' />
+    <div style={{ height: 'auto' }}>
+      <AgGridReact
+        rowData={rows}
+        columnDefs={colDefs}
+        domLayout='autoHeight'
+        theme={themeQuartz}
+      />
     </div>
   );
 };
